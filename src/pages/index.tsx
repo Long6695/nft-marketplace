1fd25c118197d7e4da6f0461cc9ad535c18b786c
Long6695 <thaihothanhlong95@gmail.com>
2023-02-25 09:39:36 +0700

    amend: remove unused code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,8 @@
+import React from 'react'
 import UIHeroSection from '@/components/Home/UIHeroSection'
 import UITrendingCollection from '@/components/Home/UITrendingCollection'
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
 
 export default function Home(): JSX.Element {
-  const [data, setData] = useState([])
-  useEffect(() => {
-    const fetchData = async (): Promise<any> => {
-      const response = await axios.get(
-        'https://jsonplaceholder.typicode.com/asdastodos/1',
-      )
-      setData(response.data)
-    }
-    fetchData().catch((err) => {
-      console.log(err)
-    })
-  }, [])
-  console.log(data)
   return (
     <div className="flex flex-col gap-[160px]">
       <UIHeroSection />
